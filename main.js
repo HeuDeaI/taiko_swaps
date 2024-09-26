bf6096e8ee1fad2192c42302390ea66dcf63ae74
Tsimka <p.profitpt@gmail.com>
2024-09-26 10:12:18 +0300

    replace 10 to 14

--- main.js
+++ main.js
@@ -168,4 +168,4 @@ async function runMultipleTimes(times) {
   );
 }
 
-runMultipleTimes(10); // More than 14 iterations achieve daily limit
+runMultipleTimes(14); // More than 14 iterations achieve daily limit
